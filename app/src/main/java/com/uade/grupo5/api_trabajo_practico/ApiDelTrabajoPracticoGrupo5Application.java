dac9b434cb7f86367869430959d32684600347dd
tomydiaz <tomydiaz739@gmail.com>
2024-08-13 21:42:45 -0300

    Se agrega estructura inicial del proyecto.

---
+++ app/src/main/java/com/uade/grupo5/api_trabajo_practico/ApiDelTrabajoPracticoGrupo5Application.java
@@ -0,0 +1,13 @@
+package com.uade.grupo5.api_trabajo_practico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDelTrabajoPracticoGrupo5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDelTrabajoPracticoGrupo5Application.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/uade/grupo5/api_trabajo_practico/ApiDelTrabajoPracticoGrupo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.grupo5.api_trabajo_practico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDelTrabajoPracticoGrupo5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
