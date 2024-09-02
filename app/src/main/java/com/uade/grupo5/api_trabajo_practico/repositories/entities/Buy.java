fb7de611882f78e361c496e71ad098b1efe4f3ff
santiago <santiagonahuellopez@gmail.com>
2024-09-01 23:11:39 -0300

    Se crea entidad basica de compra.

---
+++ app/src/main/java/com/uade/grupo5/api_trabajo_practico/repositories/entities/Buy.java
@@ -0,0 +1,10 @@
+public class Buy {
+  private long id;
+  private long charId;
+  private char* buyDate;
+
+  public constructor (charId, buyDate){
+    this.charId = charId;
+    this.buyDate = buyDate;
+  }
+}
