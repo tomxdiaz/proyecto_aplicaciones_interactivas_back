c7e4d14887495643692af6de70a1c06072bc4fa2
tomydiaz <tomydiaz739@gmail.com>
2024-09-17 21:46:34 -0300

    mockup rename to uppercase

--- src/main/java/com/uade/grupo5/api_trabajo_practico/controllers/mockupController.java
+++ src/main/java/com/uade/grupo5/api_trabajo_practico/controllers/mockupController.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,70 +20,64 @@ import com.uade.grupo5.api_trabajo_practico.services.UserService;
 
 @RestController
 @RequestMapping("/mockup")
-public class mockupController {
+public class MockupController {
 
-    @Autowired
-    private BuyService buyService;
+        @Autowired
+        private BuyService buyService;
 
-    @Autowired
-    private ProductService productService;
-    /* private CartService cartService; */
-    @Autowired
-    private UserService userService;
+        @Autowired
+        private ProductService productService;
+        
+        // @Autowired
+        /* private CartService cartService; */
 
-    @PostMapping("/initialize")
-    public ResponseEntity<Void> initializeDB() throws Exception {
+        @Autowired
+        private UserService userService;
 
-        productService.createProduct(new Product(null, "Royal Canin adulto 3KG", "Royal Canin adulto mordida chica 3KG",
-                15.00, Arrays.asList("https://http2.mlstatic.com/D_NQ_NP_835947-MLA54584711620_032023-O.webp"), "", 50,
-                false,
-                null));
-        productService
-                .createProduct(new Product(null, "Royal Canin adulto 15KG", "Royal Canin adulto mordida grande 15KG",
-                        45.00, Arrays.asList("https://http2.mlstatic.com/D_NQ_NP_965386-MLA31011602147_062019-O.webp"),
-                        "", 30, false,
-                        null));
-        productService.createProduct(new Product(null, "Dog Chow adulto 3KG", "Dog Chow adulto mordida chica 3KG",
-                12.00,
-                Arrays.asList(
-                        "https://petbaar.vtexassets.com/arquivos/ids/159373-150-auto?v=638340990045330000&width=150&height=auto&aspect=true"),
-                "", 45, false, null));
-        productService
-                .createProduct(new Product(null, "Dog Chow adulto 15KG", "Dog Chow adulto mordida grande 15KG", 42.00,
-                        Arrays.asList("https://statics.dinoonline.com.ar/imagenes/full_600x600_ma/2520746_f.jpg"), "",
-                        28, false,
-                        null));
-        productService.createProduct(new Product(null, "Guante cepillo",
-                "Cepillo Guante True Touch Mascotas Sacapelos Y Masajeador", 8.00,
-                Arrays.asList("https://http2.mlstatic.com/D_NQ_NP_810639-MLU70500938624_072023-O.webp"), "", 15, false,
-                null));
+        @GetMapping("/initialize")
+        public ResponseEntity<Void> initializeDB() throws Exception {
 
-        userService.createUser(new User(null, "Ivan", "Ivan", "Natucce", "anatucce@uade.edu.ar",
-                new SimpleDateFormat("dd/MM/yyyy").parse("04/06/1999"), "1234", "Admin", new ArrayList<>(),
-                new ArrayList<>(),
-                new ArrayList<>()));
-        userService.createUser(new User(null, "Joanna", "Joanna", "Lazarte", "jlazartelagos@uade.edu.ar",
-                new SimpleDateFormat("dd/MM/yyyy").parse("04/06/1999"), "1234", "Admin", new ArrayList<>(),
-                new ArrayList<>(),
-                new ArrayList<>()));
-        userService.createUser(new User(null, "Patricio", "Patricio", "Plem", "pplem@uade.edu.ar",
-                new SimpleDateFormat("dd/MM/yyyy").parse("04/06/1999"), "1234", "Admin", new ArrayList<>(),
-                new ArrayList<>(),
-                new ArrayList<>()));
-        userService.createUser(new User(null, "Santiago", "Santiago", "Lopez", "santiagonlopez@uade.edu.ar",
-                new SimpleDateFormat("dd/MM/yyyy").parse("04/06/1999"), "1234", "Admin", new ArrayList<>(),
-                new ArrayList<>(),
-                new ArrayList<>()));
-        userService.createUser(new User(null, "Tomas", "Tomas", "Diaz", "todiaz@uade.edu.ar",
-                new SimpleDateFormat("dd/MM/yyyy").parse("04/06/1999"), "1234", "Admin", new ArrayList<>(),
-                new ArrayList<>(),
-                new ArrayList<>()));
+                productService.createProduct(new Product(null, "Royal Canin adulto 3KG",
+                                "Royal Canin adulto mordida chica 3KG", 15.00,
+                                Arrays.asList("https://http2.mlstatic.com/D_NQ_NP_835947-MLA54584711620_032023-O.webp"),
+                                "", 50, false, null));
+                productService.createProduct(new Product(null, "Royal Canin adulto 15KG",
+                                "Royal Canin adulto mordida grande 15KG", 45.00,
+                                Arrays.asList("https://http2.mlstatic.com/D_NQ_NP_965386-MLA31011602147_062019-O.webp"),
+                                "", 30, false, null));
+                productService.createProduct(new Product(null, "Dog Chow adulto 3KG",
+                                "Dog Chow adulto mordida chica 3KG", 12.00,
+                                Arrays.asList("https://petbaar.vtexassets.com/arquivos/ids/159373-150-auto?v=638340990045330000&width=150&height=auto&aspect=true"),
+                                "", 45, false, null));
+                productService.createProduct(new Product(null, "Dog Chow adulto 15KG",
+                                "Dog Chow adulto mordida grande 15KG", 42.00,
+                                Arrays.asList("https://statics.dinoonline.com.ar/imagenes/full_600x600_ma/2520746_f.jpg"),
+                                "", 28, false, null));
+                productService.createProduct(new Product(null, "Guante cepillo",
+                                "Cepillo Guante True Touch Mascotas Sacapelos Y Masajeador", 8.00,
+                                Arrays.asList("https://http2.mlstatic.com/D_NQ_NP_810639-MLU70500938624_072023-O.webp"),
+                                "", 15, false, null));
 
-        buyService
-                .createBuy(new Buy(null, new SimpleDateFormat("dd/MM/yyyy").parse("15/09/2024"),
-                        userService.getUserById(2L)));
+                userService.createUser(new User(null, "Ivan", "Ivan", "Natucce", "anatucce@uade.edu.ar",
+                                new SimpleDateFormat("dd/MM/yyyy").parse("04/06/1999"), "1234", "Admin",
+                                new ArrayList<>(), new ArrayList<>(), new ArrayList<>()));
+                userService.createUser(new User(null, "Joanna", "Joanna", "Lazarte", "jlazartelagos@uade.edu.ar",
+                                new SimpleDateFormat("dd/MM/yyyy").parse("04/06/1999"), "1234", "Admin",
+                                new ArrayList<>(), new ArrayList<>(), new ArrayList<>()));
+                userService.createUser(new User(null, "Patricio", "Patricio", "Plem", "pplem@uade.edu.ar",
+                                new SimpleDateFormat("dd/MM/yyyy").parse("04/06/1999"), "1234", "Admin",
+                                new ArrayList<>(), new ArrayList<>(), new ArrayList<>()));
+                userService.createUser(new User(null, "Santiago", "Santiago", "Lopez", "santiagonlopez@uade.edu.ar",
+                                new SimpleDateFormat("dd/MM/yyyy").parse("04/06/1999"), "1234", "Admin",
+                                new ArrayList<>(), new ArrayList<>(), new ArrayList<>()));
+                userService.createUser(new User(null, "Tomas", "Tomas", "Diaz", "todiaz@uade.edu.ar",
+                                new SimpleDateFormat("dd/MM/yyyy").parse("04/06/1999"), "1234", "Admin",
+                                new ArrayList<>(), new ArrayList<>(), new ArrayList<>()));
 
-        return ResponseEntity.status(HttpStatus.OK).body(null);
-    }
+                buyService.createBuy(new Buy(null, new SimpleDateFormat("dd/MM/yyyy").parse("15/09/2024"),
+                                userService.getUserById(2L)));
+
+                return ResponseEntity.status(HttpStatus.OK).body(null);
+        }
 
 }
