ad614a0c1801b05621ebc60cfd40df519393d26f
tomydiaz <tomydiaz739@gmail.com>
2024-09-21 23:38:12 -0300

    Remove @ElementCollection

--- src/main/java/com/uade/grupo5/api_trabajo_practico/repositories/entities/Product.java
+++ src/main/java/com/uade/grupo5/api_trabajo_practico/repositories/entities/Product.java
@@ -25,7 +25,6 @@ public class Product {
     private String title;
     private String description;
     private double price;
-    @ElementCollection
     private List<String> images;
     private String additionalInfo;
     private int stock;
