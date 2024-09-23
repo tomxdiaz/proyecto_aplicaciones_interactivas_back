b304cddc11bf7339d3f0a7ee620fc521e202a327
tomydiaz <tomydiaz739@gmail.com>
2024-09-23 10:58:40 -0300

    remove unused lines

--- src/main/java/com/uade/grupo5/api_trabajo_practico/controllers/AuthenticationController.java
+++ src/main/java/com/uade/grupo5/api_trabajo_practico/controllers/AuthenticationController.java
@@ -9,9 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.uade.grupo5.api_trabajo_practico.repositories.entities.AuthenticationRequest;
-import com.uade.grupo5.api_trabajo_practico.repositories.entities.AuthenticationResponse;
 import com.uade.grupo5.api_trabajo_practico.repositories.entities.RegisterRequest;
 import com.uade.grupo5.api_trabajo_practico.services.AuthenticationService;
 
@@ -19,28 +17,28 @@ import com.uade.grupo5.api_trabajo_practico.services.AuthenticationService;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
-    @Autowired
-    private final AuthenticationService authService;
+  @Autowired
+  private final AuthenticationService authService;
 
-    @PostMapping("/register")
-    public ResponseEntity<?> register(
-            @RequestBody RegisterRequest request) {
-      try{
-        return ResponseEntity.status(HttpStatus.OK).body(authService.register(request));
-      }catch(Exception error){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error.getMessage());
-      }
-      
+  @PostMapping("/register")
+  public ResponseEntity<?> register(
+      @RequestBody RegisterRequest request) {
+    try {
+      return ResponseEntity.status(HttpStatus.OK).body(authService.register(request));
+    } catch (Exception error) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error.getMessage());
     }
 
-    @PostMapping("/authenticate")
-    public ResponseEntity<?> authenticate(
-            @RequestBody AuthenticationRequest request) {
-      try{
-        return ResponseEntity.status(HttpStatus.OK).body(authService.authenticate(request));
-      }catch(Exception error){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error.getMessage());
-      }
-              
+  }
+
+  @PostMapping("/authenticate")
+  public ResponseEntity<?> authenticate(
+      @RequestBody AuthenticationRequest request) {
+    try {
+      return ResponseEntity.status(HttpStatus.OK).body(authService.authenticate(request));
+    } catch (Exception error) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error.getMessage());
     }
+
+  }
 }
