92e0109f82fd9af07f7107d1611ff611c81ed920
Patricio Plem <patorafael.2000@gmail.com>
2024-09-23 22:45:17 -0300

    -m Disabled createCart -

--- src/main/java/com/uade/grupo5/api_trabajo_practico/controllers/CartController.java
+++ src/main/java/com/uade/grupo5/api_trabajo_practico/controllers/CartController.java
@@ -1,13 +1,11 @@
 package com.uade.grupo5.api_trabajo_practico.controllers;
 
-import com.uade.grupo5.api_trabajo_practico.dto.CartDTO;
 import com.uade.grupo5.api_trabajo_practico.dto.ItemDTO;
 import com.uade.grupo5.api_trabajo_practico.repositories.entities.Cart;
 import com.uade.grupo5.api_trabajo_practico.repositories.entities.Item;
 import com.uade.grupo5.api_trabajo_practico.services.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -33,15 +31,15 @@ public class CartController {
     }
 
     // Crear carrito (invocado al crear usuario - pasa ID por parametro)
-    @PostMapping("")
-    public ResponseEntity<?> createCart(@RequestBody Long userID) {
-      try{
-        cartService.createCart(userID);
-        return ResponseEntity.status(HttpStatus.OK).body("Carrito creado correctamente!");
-      }catch(Exception error){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error.getMessage());
-      }
-    }
+//    @PostMapping("")
+//    public ResponseEntity<?> createCart(@RequestBody Long userID) {
+//      try{
+//        cartService.createCart(userID);
+//        return ResponseEntity.status(HttpStatus.OK).body("Carrito creado correctamente!");
+//      }catch(Exception error){
+//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error.getMessage());
+//      }
+//    }
 
     // Agregar un ítem al carrito
     @PostMapping("/{cartId}/item")
@@ -85,12 +83,11 @@ public class CartController {
         }catch(Exception error){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error.getMessage());
         }
-
     }
 
     // -devuelve precio total del carrito
     @GetMapping("/{cartId}/total")
-    public ResponseEntity<Double> getTotalCart(@PathVariable Long cartId) {
+    public ResponseEntity<Double> getTotalCart(@PathVariable Long cartId) throws Exception {
         return ResponseEntity.ok(cartService.getTotal(cartId));
     }
     // Obtener todos los items de un carrito
